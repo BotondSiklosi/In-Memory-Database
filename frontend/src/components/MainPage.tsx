0aea1a22341e7bf6c5702b605e47357d25851ede
Boti <kissiki99@gmail.com>
2020-10-30 10:11:33 +0100

    first push

---
+++ frontend/src/components/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainPageStyle = styled.div`
+    
+`
+
+export default function MainPage() {
+    return (
+        <MainPageStyle>
+
+        </MainPageStyle>
+    )
+}
